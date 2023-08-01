0a15160f32c9eadd9c11a0cec55d09e88399d389
Den4ik20020 <sertus659@gmail.com>
2023-08-01 19:54:54 +0500

    lesson_6

--- mysite/advertisements/models.py
+++ mysite/advertisements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    auction = models.BooleanField(help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
